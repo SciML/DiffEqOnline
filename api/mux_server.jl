01b8a9f3be38c7b7119cb2851b8812d52a4f0c21
Alex Mellnik <a.r.mellnik@gmail.com>
2017-01-10 19:20:18 -0800

    Move new server to this repo

--- api/mux_server.jl
+++ api/mux_server.jl
@@ -1,30 +1,46 @@
-# Load required packages
-using JuliaWebAPI, Logging, Compat, ZMQ, DifferentialEquations, Plots
+using DiffEqBase, OrdinaryDiffEq, ParameterizedFunctions, Plots, Mux, JSON, HttpCommon
 
-plotly()
-
-
-const SRVR_ADDR = "tcp://127.0.0.1:9999"
-const Cross_origin_JSON = Dict{Compat.UTF8String,Compat.UTF8String}("Content-Type" => "application/json; charset=utf-8", "Access-Control-Allow-Origin" => "http://localhost:4200")
-const numpoints = 1000
-
+# Handy functions
 expr_has_head(s, h) = false
 expr_has_head(e::Expr, h::Symbol) = expr_has_head(e, Symbol[h])
 function expr_has_head(e::Expr, vh::Vector{Symbol})
     in(e.head, vh) || any(a -> expr_has_head(a, vh), e.args)
 end
-
 has_function_def(s::String) = has_function_def(parse(s; raise=false))
 has_function_def(e::Expr) = expr_has_head(e, Symbol[:(->), :function])
 
-
-function squareit(b64)
-    strArr = String(base64decode(b64))
-    arr = JSON.parse(strArr)
-    return JSON.json(Dict("data" => arr.^2))
+# Headers -- set Access-Control-Allow-Origin for either dev or prod
+function withHeaders(res, req)
+    println("Origin: ", get(req[:headers], "Origin", ""))
+    headers  = HttpCommon.headers()
+    headers["Content-Type"] = "application/json; charset=utf-8"
+    if get(req[:headers], "Origin", "") == "http://localhost:4200"
+        headers["Access-Control-Allow-Origin"] = "http://localhost:4200"
+    else
+        headers["Access-Control-Allow-Origin"] = "http://app.juliadiffeq.org"
+    end
+    println(headers["Access-Control-Allow-Origin"])
+    Dict(
+       :headers => headers,
+       :body=> res
+    )
 end
 
-function solveit(b64)
+# A debug endpoint
+function squareit(req)
+    strArr = String(base64decode(convert(String, req[:path][1])))
+    arr = JSON.parse(strArr)
+    return  JSON.json(Dict("data" => arr.^2, "error" => false))
+end
+
+# The ODE endpoint
+function solveit(req::Dict{Any,Any})
+    b64 = convert(String, req[:path][1])
+    solveit(b64)
+end
+
+function solveit(b64::String)
+    plotly()
     strObj = String(base64decode(b64))
     obj = JSON.parse(strObj)
     # println(obj)
@@ -45,6 +61,14 @@ function solveit(b64)
     println("tspan: ", tspan)
     u0 = [parse(Float64, u) for u in obj["initialConditions"]]
     println("u0: ", u0)
+    algstr = obj["solver"]  #Get this from the reqest in the future!
+    algs = Dict{Symbol,OrdinaryDiffEq.OrdinaryDiffEqAlgorithm}(
+                :Tsit5 => Tsit5(),
+                :Vern6 => Vern6(),
+                :Vern7 => Vern7(),
+                :Feagin14 => Feagin14(),
+                :BS3 => BS3(),
+                :Rosenbrock23 => Rosenbrock23())
     opts = Dict{Symbol,Bool}(
         :build_tgrad => true,
         :build_jac => true,
@@ -55,15 +79,17 @@ function solveit(b64)
         :build_invhes => false,
         :build_dpfuncs => true)
     f = ode_def_opts(name, opts, ex, params...)
-    println("did f")
     prob = ODEProblem(f,u0,tspan)
-    println("did prob: ", prob)
-    sol = solve(prob)
+    alg = algs[parse(algstr)]
+    sol = solve(prob,alg);
 
     println("did sol")
+    numpoints = 1000
     newt = collect(linspace(sol.t[1],sol.t[end],numpoints))
     newu = sol.interp(newt)
+    println("did interp")
     p = plot(sol,xlabel="t")
+    println("did plot")
     layout = Plots.plotly_layout_json(p)
     series = Plots.plotly_series_json(p)
 
@@ -76,12 +102,16 @@ function solveit(b64)
 
     res = Dict("u" => newu, "t" => newt, "layout" =>layout, "series"=>series)
     return JSON.json(Dict("data" => res, "error" => false))
-
 end
 
-const REGISTERED_APIS = [
-        (squareit, false, Cross_origin_JSON),
-        (solveit, false, Cross_origin_JSON)
-]
+@app test = (
+    Mux.defaults,
+    page(respond("Nothing to see here...")),
+    route("/squareit", req -> withHeaders(squareit(req), req)),
+    route("/solveit", req -> withHeaders(solveit(req), req)),
+    route("/test", req -> withHeaders("Meh", req)),
+    Mux.notfound()
+)
 
-process(REGISTERED_APIS, "tcp://127.0.0.1:9999"; bind=true, log_level=INFO)
+println("About to start the server")
+@sync serve(test, port=parse(Int64, ARGS[1]))
--- api/run_api.jl
+++
@@ -1,9 +0,0 @@
-using JuliaWebAPI   #Load package
-using Logging
-Logging.configure(level=INFO);
-
-#Create the ZMQ client that talks to the ZMQ listener above
-const apiclnt = APIInvoker("tcp://127.0.0.1:9999");
-
-#Starts the HTTP server in current process
-run_http(apiclnt, 7777)
